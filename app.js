969eaadfac26afe30f61157ec289fc8dff176b35
HatimKhetty <hatimfkhetty53@gmail.com>
2021-03-02 08:56:25 +0500

    FMP-project-complete

---
+++ app.js
@@ -0,0 +1,17 @@
+function login() {
+
+
+    
+
+
+};
+
+
+function signup() {
+
+    
+};
+
+function addtocard() {
+    alert("Sorry Add To Card Feature IS Not Updated Write Now PLease Contact Us on Whatsapp with item ScreenShot, Otherwise Contact us on Intagram or facebook....!")
+} 
